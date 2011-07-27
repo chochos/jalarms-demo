afac77cbc13a5b0aa66e9676efee2713d15f48fe
Enrique Zamudio <alterbass@gmail.com>
2011-07-26 23:37:51 -0500

    Agregar mas dependencias faltantes en testRuntime. Ya pasan las pruebas, asi queda lista para iniciar la demo.

--- src/main/resources/insertar_datos.sql
+++ src/main/resources/insertar_datos.sql
@@ -1,4 +1,6 @@
 INSERT INTO maestro (nombre) values ('Maestro');
 
 INSERT INTO detalle (master, detalle) values (1, 'Detalle 1');
+INSERT INTO detalle (master, detalle) values (1, 'Detalle 2');
+INSERT INTO detalle (master, detalle) values (1, 'Detalle 3');
 
